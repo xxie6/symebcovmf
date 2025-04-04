1cff9ffd8372d6f1949efb85bf77950bced44411
Annie Xie <xxie306@gmail.com>
2025-04-04 11:53:28 -0500

    Add function to refit lambdas and edit fit functions to allow for this

---
+++ R/refit_lambda.R
@@ -0,0 +1,88 @@
+#' Refit lambda
+#'
+#' @param S An n-by-n Gram/covariance matrix
+#' @param sym_ebcovmf_obj A symEBcovMF object
+#' @param maxiter The maximum number of iterations for optimization
+#' @param tol The convergence tolerance parameter for optimization
+#'
+#' @return A symEBcovMF object
+#' @export
+#'
+refit_lambda <- function(S, sym_ebcovmf_obj, maxiter = 100, tol = 10^(-6)){
+  K <- length(sym_ebcovmf_obj$lambda)
+  if (K <= 1){
+    print('Cannot refit lambda')
+  } else {
+    sym_ebcovmf_obj.old <- sym_ebcovmf_obj
+    iter <- 1
+    obj_diff <- Inf
+    curr_elbo <- -Inf
+    while((iter <= maxiter) && (obj_diff > tol)){
+      print(iter)
+      # Update lambdas
+      lambda.old <- sym_ebcovmf_obj$lambda
+      for (k in 1:K){
+        Rk <- S - tcrossprod(sym_ebcovmf_obj$L_pm[,-k] %*% diag(sqrt(sym_ebcovmf_obj$lambda[-k]), ncol = (K-1)))
+        sym_ebcovmf_obj$lambda[k] <- max(t(sym_ebcovmf_obj$L_pm[,k, drop = FALSE]) %*% Rk %*% sym_ebcovmf_obj$L_pm[,k, drop = FALSE], 0)
+      }
+
+      # Update resid_s2
+      resid_s2.old <- sym_ebcovmf_obj$resid_s2
+      sym_ebcovmf_obj$resid_s2 <- estimate_resid_s2(S = S,
+                                    L = sym_ebcovmf_obj$L_pm,
+                                    lambda = sym_ebcovmf_obj$lambda,
+                                    n = sym_ebcovmf_obj$n,
+                                    K = K)
+
+      # Update elbo
+      curr_elbo.old <- curr_elbo
+      curr_elbo <- compute_elbo(S = S,
+                                L = sym_ebcovmf_obj$L_pm,
+                                lambda = sym_ebcovmf_obj$lambda,
+                                resid_s2 = sym_ebcovmf_obj$resid_s2,
+                                n = sym_ebcovmf_obj$n,
+                                K = K,
+                                KL = sym_ebcovmf_obj$KL)
+
+      # Check convergence
+      if (iter > 1){
+        obj_diff <- curr_elbo - curr_elbo.old
+      }
+      if (obj_diff < 0){
+        sym_ebcovmf_obj$lambda <- lambda.old
+        sym_ebcovmf_obj$resid_s2 <- resid_s2.old
+        curr_elbo <- curr_elbo.old
+        print(paste('elbo decreased by', abs(obj_diff)))
+        break
+      }
+      iter <- iter + 1
+    }
+    # nullcheck
+    if (any(sym_ebcovmf_obj$lambda == 0)){
+      idx <- which(sym_ebcovmf_obj$lambda != 0)
+      sym_ebcovmf_obj$lambda <- sym_ebcovmf_obj$lambda[idx]
+      sym_ebcovmf_obj$L_pm <- sym_ebcovmf_obj$L_pm[,idx]
+      sym_ebcovmf_obj$KL <- sym_ebcovmf_obj$KL[idx]
+      sym_ebcovmf_obj$resid_s2 <- estimate_resid_s2(S = S,
+                                                    L = sym_ebcovmf_obj$L_pm,
+                                                    lambda = sym_ebcovmf_obj$lambda,
+                                                    n = sym_ebcovmf_obj$n,
+                                                    K = K)
+      curr_elbo <- compute_elbo(S = S,
+                                L = sym_ebcovmf_obj$L_pm,
+                                lambda = sym_ebcovmf_obj$lambda,
+                                resid_s2 = sym_ebcovmf_obj$resid_s2,
+                                n = sym_ebcovmf_obj$n,
+                                K = K,
+                                KL = sym_ebcovmf_obj$KL)
+    }
+    # check objective function
+    if ((curr_elbo - sym_ebcovmf_obj.old$elbo) < 0){
+      sym_ebcovmf_obj <- sym_ebcovmf_obj.old
+    } else {
+      sym_ebcovmf_obj$elbo <- curr_elbo
+      sym_ebcovmf_obj$vec_elbo_K[K] <- curr_elbo
+    }
+  }
+  return(sym_ebcovmf_obj)
+}
--- R/sym_ebcovmf_fit.R
+++ R/sym_ebcovmf_fit.R
@@ -1,36 +1,40 @@
 #' Fit rank K symEBcovMF model
-#' 
+#'
 #' @param S A n-by-n Gram/covariance matrix
 #' @param ebnm_fn An EBNM solver
 #' @param Kmax The maximum number of factors to add
 #' @param maxiter The maximum number of iterations used when optimizing the rank one fit
 #' @param rank_one_tol The convergence tolerance for the rank one fit
 #' @param tol The convergence tolerance for the rank K fit
-#' 
+#' @param refit_lam True or False for if you want to refit the lambda values after each factor is added
+#'
 #' @return A symEBcovMF object
 #' @export
-#' 
-sym_ebcovmf_fit <- function(S, ebnm_fn, Kmax, maxiter, rank_one_tol, tol){
+#'
+sym_ebcovmf_fit <- function(S, ebnm_fn, Kmax, maxiter, rank_one_tol, tol, refit_lam = FALSE){
   #initialize object
   sym_ebcovmf_obj <- sym_ebcovmf_init(S)
-  
+
   curr_rank <- 0
   obj_diff <- Inf
   while ((curr_rank < Kmax) & (obj_diff > tol)){
     # add factor
     sym_ebcovmf_obj <- sym_ebcovmf_r1_fit(S, sym_ebcovmf_obj, ebnm_fn, maxiter, rank_one_tol)
-    
+
     # check if new factor was added
     if (length(sym_ebcovmf_obj$vec_elbo_K) == curr_rank){
       print(paste('Adding factor', (curr_rank + 1), 'does not improve the objective function'))
       break
     } else {
       if (curr_rank > 0){
+        if (refit_lam == TRUE){
+          sym_ebcovmf_obj <- refit_lambda(S, sym_ebcovmf_obj)
+        }
         obj_diff <- sym_ebcovmf_obj$vec_elbo_K[curr_rank + 1] - sym_ebcovmf_obj$vec_elbo_K[curr_rank]
       }
     }
     curr_rank <- curr_rank + 1
   }
-  
+
   return(sym_ebcovmf_obj)
 }
