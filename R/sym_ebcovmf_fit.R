9a51921dbaf1f65025b7e06552b81ce48e2cec5b
Annie Xie <xxie306@gmail.com>
2026-02-18 10:19:56 -0600

    Update messages for sym_ebcovmf_fit output

--- R/sym_ebcovmf_fit.R
+++ R/sym_ebcovmf_fit.R
@@ -20,6 +20,7 @@ sym_ebcovmf_fit <- function(S, ebnm_fn, Kmax, maxiter, rank_one_tol, tol, sign_c
   obj_diff <- Inf
   while ((curr_rank < Kmax) & (obj_diff > tol)){
     # add factor
+    print(paste('Adding factor', (curr_rank + 1)))
     sym_ebcovmf_obj <- sym_ebcovmf_r1_fit(S, sym_ebcovmf_obj, ebnm_fn, maxiter, rank_one_tol, sign_constraint = sign_constraint)
 
     # check if new factor was added
@@ -32,6 +33,9 @@ sym_ebcovmf_fit <- function(S, ebnm_fn, Kmax, maxiter, rank_one_tol, tol, sign_c
           sym_ebcovmf_obj <- refit_lambda(S, sym_ebcovmf_obj)
         }
         obj_diff <- sym_ebcovmf_obj$vec_elbo_K[curr_rank + 1] - sym_ebcovmf_obj$vec_elbo_K[curr_rank]
+        if (obj_diff < tol){
+          print(paste0('The objective function difference is: ', obj_diff, '. We will stop adding factors.' ))
+        }
       }
     }
     curr_rank <- curr_rank + 1
