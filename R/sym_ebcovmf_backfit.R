d5821985c32c9a58d67c43d5bf1972e3a33e7754
Annie Xie <xxie306@gmail.com>
2026-06-02 14:51:08 -0500

    update backfit function to be pipeable

--- R/sym_ebcovmf_backfit.R
+++ R/sym_ebcovmf_backfit.R
@@ -11,29 +11,36 @@
 #' @return A symEBcovMF object
 #' @export
 #'
-sym_ebcovmf_backfit <- function(S, sym_ebcovmf_obj, ebnm_fn, backfit_maxiter = 100, backfit_tol = 10^(-8), optim_maxiter= 500, optim_tol = 10^(-8)){
+sym_ebcovmf_backfit <- function(sym_ebcovmf_obj, ebnm_fn, backfit_maxiter = 100, backfit_tol = 10^(-8), optim_maxiter= 500, optim_tol = 10^(-8)){
   K <- length(sym_ebcovmf_obj$lambda)
   iter <- 1
   obj_diff <- Inf
   sym_ebcovmf_obj$backfit_vec_elbo_full <- NULL
   sym_ebcovmf_obj$backfit_iter_elbo_vec <- NULL
 
-  # refit lambda
-  # sym_ebcovmf_obj <- refit_lambda(S, sym_ebcovmf_obj, maxiter = 25)
-
   while((iter <= backfit_maxiter) && (obj_diff > backfit_tol)){
-    # print(iter)
     obj_old <- sym_ebcovmf_obj$elbo
     # loop through each factor
     for (k in 1:K){
-      # print(k)
       print(paste('Updating factor', k))
+
       # compute residual matrix
-      R <- S - tcrossprod(sym_ebcovmf_obj$L_pm[,-k, drop = FALSE] %*% diag(sqrt(sym_ebcovmf_obj$lambda[-k]), ncol = (K-1)))
-      R2k <- compute_R2(S, sym_ebcovmf_obj$L_pm[,-k, drop = FALSE], sym_ebcovmf_obj$lambda[-k], (K-1)) #this is right but I have one instance where the values don't match what I expect
+      R <- sym_ebcovmf_obj$S - tcrossprod(sym_ebcovmf_obj$L_pm[,-k, drop = FALSE] %*% diag(sqrt(sym_ebcovmf_obj$lambda[-k]), ncol = (K-1)))
+      R2k <- compute_R2(sym_ebcovmf_obj$S,
+                        sym_ebcovmf_obj$L_pm[,-k, drop = FALSE],
+                        sym_ebcovmf_obj$lambda[-k], (K-1))
 
       # optimize factor
-      factor_proposed <- optimize_factor(R, ebnm_fn, optim_maxiter, optim_tol, sym_ebcovmf_obj$L_pm[,k], sym_ebcovmf_obj$lambda[k], sym_ebcovmf_obj$fitted_gs[[k]], R2k, sym_ebcovmf_obj$n, sym_ebcovmf_obj$KL[-k])
+      factor_proposed <- optimize_factor(R,
+                                         ebnm_fn,
+                                         optim_maxiter,
+                                         optim_tol,
+                                         sym_ebcovmf_obj$L_pm[,k],
+                                         sym_ebcovmf_obj$lambda[k],
+                                         sym_ebcovmf_obj$fitted_gs[[k]],
+                                         R2k,
+                                         sym_ebcovmf_obj$n,
+                                         sym_ebcovmf_obj$KL[-k])
 
       # update object
       # check if update leads to increase in objective function
@@ -49,10 +56,6 @@ sym_ebcovmf_backfit <- function(S, sym_ebcovmf_obj, ebnm_fn, backfit_maxiter = 1
         obj_diff <- sym_ebcovmf_obj$elbo - factor_proposed$curr_elbo
         print(paste('update to factor', k, 'decreased the elbo by', abs(obj_diff)))
       }
-
-      #print(sym_ebcovmf_obj$elbo)
-      # sym_ebcovmf_obj <- refit_lambda(S, sym_ebcovmf_obj) # add refitting step?
-      #print(sym_ebcovmf_obj$elbo)
     }
     sym_ebcovmf_obj$backfit_iter_elbo_vec <- c(sym_ebcovmf_obj$backfit_iter_elbo_vec, sym_ebcovmf_obj$elbo)
 
